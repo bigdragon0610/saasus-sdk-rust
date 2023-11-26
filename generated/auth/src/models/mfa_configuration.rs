747e3518d357a77324bf9c712fe68718d4e26626
ryudai <bigdragon.06102001@gmail.com>
2023-11-27 01:55:58 +0900

    mfa_configurationのバグを修正

--- generated/auth/src/models/mfa_configuration.rs
+++ generated/auth/src/models/mfa_configuration.rs
@@ -16,12 +16,12 @@
 pub struct MfaConfiguration {
     /// on: 全ユーザーがログイン時に適用(apply when all users log in) optional: MFA要素が有効になっている個別ユーザーに適用(apply to individual users with MFA factor enabled) ※ パラメータは現在optionalで固定となります。(The parameter is currently optional and fixed.) 
     #[serde(rename = "mfa_configuration")]
-    pub mfa_configuration: MfaConfiguration,
+    pub mfa_configuration: MfaConfigurationEnum,
 }
 
 impl MfaConfiguration {
     /// MFAデバイス認証設定(MFA device authentication settings) ※ 未提供の機能のため、変更・保存はできません(This function is not yet provided, so it cannot be changed or saved.) 
-    pub fn new(mfa_configuration: MfaConfiguration) -> MfaConfiguration {
+    pub fn new(mfa_configuration: MfaConfigurationEnum) -> MfaConfiguration {
         MfaConfiguration {
             mfa_configuration,
         }
@@ -30,15 +30,15 @@ impl MfaConfiguration {
 
 /// on: 全ユーザーがログイン時に適用(apply when all users log in) optional: MFA要素が有効になっている個別ユーザーに適用(apply to individual users with MFA factor enabled) ※ パラメータは現在optionalで固定となります。(The parameter is currently optional and fixed.) 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
-pub enum MfaConfiguration {
+pub enum MfaConfigurationEnum {
     #[serde(rename = "on")]
     On,
     #[serde(rename = "optional")]
     Optional,
 }
 
-impl Default for MfaConfiguration {
-    fn default() -> MfaConfiguration {
+impl Default for MfaConfigurationEnum {
+    fn default() -> MfaConfigurationEnum {
         Self::On
     }
 }
